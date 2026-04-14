2cce6f0b68fbdb1d97fdd4815f2372cd13f1355a
saidansaf <afzalxonovsaidansaf@gmail.com>
2026-04-14 18:54:12 +0500

    index.htmlga ulash

--- app1/views.py
+++ app1/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render
-
 # Create your views here.
-
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def hello_world(request):
-    return HttpResponse("Hello World")
+    return render(request,'index.html')
