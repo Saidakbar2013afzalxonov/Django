bbb1c352ceee917f03a280e0536f98c606a57eb1
DostoevskiyRiuui <Hobbycool9@gmail.com>
2026-04-15 19:42:53 +0500

    ozgarish

--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,14 @@
 # Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
 
-def hello_world(request):
-    return render(request,'index.html')
+
+def abc(request):
+    users=[
+        {'ism':'Saidakbar','familiya':'Avzalxonov','yosh':12},
+        {'ism':'Saidansaf','familiya':'Avzalxonov','yosh':11},
+    ]
+
+    users=models.User.objects.all()
+    return render(request, 'dtl.html',context={'users':users})
--- project/urls.py
+++ project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/',include('app1.urls')),
+    path('abc/',include('app1.urls'))
 ]
